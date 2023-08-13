fd6df693e30f8580a7a45cd7189a8803460cb2b2
SpaceBuckets <agustin.capeletto@agustin.capeletto>
2023-08-13 16:00:49 -0300

    a

--- api/api.js
+++ api/api.js
@@ -1,52 +1,31 @@
-exports.handler = async (event, context) => {
+const fetchData = async kpi => await (await fetch(`${process.env.URL}/data/${kpi}.json`)).json();
+
+const filter = (data, start, end) => data.filter(item => (!start || item.x >= start) && (!end || item.x <= end));
+
+const aggregate = (data, period, fruc) => Object.entries(data.reduce((groups, item) => {
+  const key = item.x.substr(0, period);
+  groups[key] = groups[key] || { sum: 0, count: 0 };
+  groups[key].sum += item.y;
+  groups[key].count++;
+  return groups;
+}, {})).map(([k, v]) => ({ x: k, y: fruc === 'mean' ? (v.sum / v.count).toFixed(2) : v.sum.toFixed(2) }));
+
+exports.handler = async event => {
   try {
-    const { queryStringParameters } = event;
-    const { kpi: kpiName, startDate, endDate, frec } = queryStringParameters;
+    const { kpi, startDate: start, endDate: end, frec } = event.queryStringParameters;
+    if (!kpi) return { statusCode: 400, body: JSON.stringify({ error: 'KPI name is missing' }) };
+    const fileData = await fetchData(kpi);
+    let outputData = {};
 
-    if (!kpiName) {
-      return { statusCode: 400, body: JSON.stringify({ error: 'KPI name is missing' }) };
+    for (const dimension in fileData.dimensions) {
+      let data = fileData.dimensions[dimension].data;
+      if (start || end) data = filter(data, start, end);
+      if (frec && ['Diaria', 'Mensual'].includes(fileData.frec)) data = aggregate(data, frec.toLowerCase() === 'anual' ? 4 : 7, fileData.fruc);
+      outputData[dimension] = data;
     }
 
-    const filePath = `${process.env.URL}/data/${kpiName}.json`;
-
-    const fetchResponse = await fetch(filePath);
-    if (!fetchResponse.ok) {
-      return { statusCode: 404, body: JSON.stringify({ error: 'KPI not found' }) };
-    }
-
-    const fileData = await fetchResponse.json();
-    let outputData = fileData?.dimensions || {};
-
-    const aggregate = (arr, period) => arr.reduce((groups, item) => {
-      const key = item.x.substr(0, period);
-      groups[key] = groups[key] || { sum: 0, count: 0 };
-      groups[key].sum += item.y;
-      groups[key].count++;
-      return groups;
-    }, {});
-
-    for (const dim in outputData) {
-      if ((startDate || endDate) && outputData[dim].data.length > 0) {
-        outputData[dim].data = outputData[dim].data.filter(item => (!startDate || item.x >= startDate) && (!endDate || item.x <= endDate));
-      }
-
-      if (frec && ['Diaria', 'Mensual'].includes(fileData.frec)) {
-        const period = frec.toLowerCase() === 'anual' ? 4 : 7;
-        outputData[dim].data = Object.entries(aggregate(outputData[dim].data, period)).map(([key, value]) => ({
-          x: key,
-          y: fileData.frec === 'mean' ? (value.sum / value.count).toFixed(2) : value.sum.toFixed(2)
-        }));
-      }
-    }
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(outputData)
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal server error' })
-    };
+    return { statusCode: 200, body: JSON.stringify(outputData) };
+  } catch {
+    return { statusCode: 404, body: JSON.stringify({ error: 'KPI not found' }) };
   }
 };
