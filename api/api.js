aac5c67cd867987440ef928b40530edd1557e890
SpaceBuckets <agustin.capeletto@agustin.capeletto>
2023-08-13 18:40:32 -0300

    a

--- api/api.js
+++ api/api.js
@@ -31,7 +31,7 @@ exports.handler = async event => {
 
       outputData.push({ label, data: filteredData });
     }
-    megaData = {title: fileData.t, outputData}
+    megaData = {title: `${fileData.t}. ${fileData.st}`, dimensions: outputData}
     const headers = {
       'Access-Control-Allow-Origin': '*', // Allow requests from any origin
       'Access-Control-Allow-Headers': 'Content-Type', // Allow the Content-Type header
